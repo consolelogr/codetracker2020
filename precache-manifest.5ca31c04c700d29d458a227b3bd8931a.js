53e0f2a53ea37657d197a624a103f8bca3f1b71d
Consolelogr <consolelogger@ftml.net>
2020-08-26 17:16:47 +0300

    Updates

---
+++ precache-manifest.5ca31c04c700d29d458a227b3bd8931a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "728c64555ebf2dbef7aed954ce1dcce6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ef263d6cd59c5f3342a",
+    "url": "/static/css/main.24c420f8.chunk.css"
+  },
+  {
+    "revision": "e58663179b6ce41a28fe",
+    "url": "/static/js/2.4fa0cfbf.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/static/js/2.4fa0cfbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ef263d6cd59c5f3342a",
+    "url": "/static/js/main.c892c3e0.chunk.js"
+  },
+  {
+    "revision": "ba0bf47a82ffd6d1b8c7",
+    "url": "/static/js/runtime-main.8f0bc22a.js"
+  },
+  {
+    "revision": "db29b234209b01b0d0f30ce868f49025",
+    "url": "/static/media/bg.db29b234.png"
+  }
+]);
