8dfbd7c439112962cb00ceb76c6e113144ef0e5b
Consolelogr <consolelogger@ftml.net>
2020-08-26 17:26:24 +0300

    Updates

---
+++ precache-manifest.adc4c35c8bc81dd514083e2584efc9f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7202b11c419ed54e45277f3982ac3deb",
+    "url": "/codetracker2020/index.html"
+  },
+  {
+    "revision": "00e03c3ccb9a7b5f0d01",
+    "url": "/codetracker2020/static/css/main.7d92824d.chunk.css"
+  },
+  {
+    "revision": "e58663179b6ce41a28fe",
+    "url": "/codetracker2020/static/js/2.4fa0cfbf.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/codetracker2020/static/js/2.4fa0cfbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00e03c3ccb9a7b5f0d01",
+    "url": "/codetracker2020/static/js/main.7c565756.chunk.js"
+  },
+  {
+    "revision": "493d78e85193729c15e3",
+    "url": "/codetracker2020/static/js/runtime-main.08b19379.js"
+  },
+  {
+    "revision": "db29b234209b01b0d0f30ce868f49025",
+    "url": "/codetracker2020/static/media/bg.db29b234.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5ca31c04c700d29d458a227b3bd8931a.js"
+  "/codetracker2020/precache-manifest.adc4c35c8bc81dd514083e2584efc9f3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/codetracker2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
